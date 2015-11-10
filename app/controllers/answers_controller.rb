eccfac5e73c85a26d63a13050d5318dc9577225f
rajangdavis <rajangdavis@gmail.com>
2015-11-09 20:58:38 -0800

    going to limit it to 10 answers, let's see if this works

--- app/controllers/answers_controller.rb
+++ app/controllers/answers_controller.rb
@@ -35,7 +35,7 @@ class AnswersController < ApplicationController
   	end
 
   	def spreadsheet
-  		@answers = Answer.all
+  		@answers = Answer.find(:all, :limit => 10)
   		@steps = Step.find(:all, :order => 'number')
   		respond_to do |format|
   			format.xls
