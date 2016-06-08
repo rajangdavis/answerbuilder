8357e9db2597fce7557f2b931c69af70669da0dd
rajangdavis <rajangdavis@gmail.com>
2016-06-07 19:24:48 -0700

    added in a new firebase thing, set it up with a wysiwyg editor that I might change. Trying to set up something so that I can have asynchronous updates that go onto the support portal

--- public/qsee_updates.js
+++ public/qsee_updates.js
@@ -1,12 +1,15 @@
 var url = "https://qseeupdates.firebaseio.com/";
 var firebase = new Firebase(url);
-var app = angular.module('qsee_updates',['ui.router','firebase'])
-.controller('qsee_updates_ctrl',function($scope,$timeout,$state){
-	
+var posts = new Firebase(url+"posts");
+
+var app = angular.module('qsee_updates',['ui.router','firebase','angularTrix'])
+.controller('qsee_updates_ctrl',function($scope,$timeout,$state,$firebaseArray){
+
 	function _init(){
 		$scope.authData = firebase.getAuth();
+		$scope.posts = $firebaseArray(posts);
 	}
-
+	
 	_init();
 
 	$scope.error = undefined;
@@ -15,6 +18,14 @@ var app = angular.module('qsee_updates',['ui.router','firebase'])
 		$scope.loggedIn = true;
 	}else{
 		$scope.loggedIn = false;
+	}
+
+	$scope.checkIfLoggedIn = function(){
+		$timeout(function(){
+			if(!$scope.loggedIn){
+				$state.go('login');
+			}
+		});
 	}
 
 	function authDataCallback(authData) {
@@ -57,6 +68,22 @@ var app = angular.module('qsee_updates',['ui.router','firebase'])
         $scope.loggedIn = false;
         $state.go('login');
 	}    
+
+	$scope.createPost = function(title,content){
+		var newPost = posts.push();
+		newPost.set({
+			title: title,
+			content: content,
+			id:newPost.path.o[1]
+		}, function(error) {
+			if (error) {
+				console.log("Data could not be saved." + error);
+			} else {
+				$state.go('show',{id:newPost.path.o[1]});
+			}
+		});
+	}
+
 })
 .config(function($stateProvider, $urlRouterProvider) {
 	$urlRouterProvider.otherwise("/login");
@@ -66,19 +93,53 @@ var app = angular.module('qsee_updates',['ui.router','firebase'])
     	templateUrl:'partials/login.html',
     	controller: function($scope,$state){
     		if($scope.loggedIn){
-    			$state.go('home');
+    			$state.go('home')
     		}
-    	}
+    	}	
 	})
 	.state('home', {
     	url: "/home",
     	templateUrl:'partials/list.html',
     	controller: function($scope,$state,$timeout){
-    		$timeout(function(){
-    			if(!$scope.loggedIn){
-					$state.go('login');
-				}
-    		})
+    		$scope.checkIfLoggedIn()
     	}
 	})
+	.state('new', {
+    	url: "/new",
+    	templateUrl:'partials/create.html',
+    	controller: function($scope,$state,$timeout){
+    		$scope.checkIfLoggedIn()
+    	}
+	})
+	.state('show', {
+    	url: "/show?id",
+    	templateUrl:'partials/show.html',
+    	controller: function($scope,$state,$timeout,$stateParams,$firebaseObject){
+    		$scope.checkIfLoggedIn()
+    		if($stateParams.id){
+    			var postUrl = $firebaseObject(posts.child($stateParams.id));
+    			$scope.post = postUrl.$$conf.binding.rec;
+    		}
+    	}
+	})
+})
+.directive('dynamic', function ($compile,$timeout) {
+  return {
+    restrict: 'A',
+    replace: true,
+    transclude: true,
+    link: function (scope, ele, attrs) {
+      //this is so we can have content load in and bind to the DOM via AJAX
+      scope.$watch(attrs.dynamic, function(html) {
+        var answerContainer = document.getElementById('answer-container');
+        if(answerContainer){
+          answerContainer.parentElement.removeChild(answerContainer);
+        }
+        $timeout(function(){
+          ele.html(html);
+          $compile(ele.contents())(scope)
+        })  
+      });
+    }
+  };
 })
