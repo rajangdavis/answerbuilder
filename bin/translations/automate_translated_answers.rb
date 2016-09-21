49acbd54580fca0026a97bfdf9c9f0083d9cb312
rajangdavis <rajangdavis@gmail.com>
2016-09-20 19:16:48 -0700

    figured it out, when I need to make code and db changes, I have to do it in qseecode 1 not 2... very interesting

--- bin/translations/automate_translated_answers.rb
+++ bin/translations/automate_translated_answers.rb
@@ -5,14 +5,14 @@ Dir.glob('*.json') do |json_file|
 	data_hash = JSON.parse(file)
 	puts "Starting to update #{data_hash['id']}"
 	answer = Answer.find(data_hash['id'])
-	answer.fr_title = data_hash['title']
-	answer.fr_tagline = data_hash['tagline']
+	answer.title_fr = data_hash['title']
+	answer.tagline_fr = data_hash['tagline']
 	answer.save!
 	puts '#{answer.id} was updated'
 	data_hash['steps'].each do |step_|
 		step = Step.find(step_['id'])
 		step.step_fr = step_['step']
-		step.image_upload = step_['image']
+		step.image_upload_fr = step_['image']
 		step.save!
 		puts '#{step.id} was updated'
 	end
