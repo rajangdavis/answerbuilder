0f49a1a5b3fe1905aa2874fe060e8532a822147a
rajangdavis <rajangdavis@gmail.com>
2016-09-20 19:44:03 -0700

    I think I may have figured it out

--- db/migrate/20160921004314_french_answer_support.rb
+++ db/migrate/20160921004314_french_answer_support.rb
@@ -2,7 +2,7 @@ class FrenchAnswerSupport < ActiveRecord::Migration
    def self.up
     add_column :answers, :title_fr, :string
     add_column :answers, :tagline_fr, :string
-    add_column :steps, :step_fr, :text
+    # add_column :steps, :step_fr, :text
     add_attachment :steps, :image_upload_fr
   end
 
