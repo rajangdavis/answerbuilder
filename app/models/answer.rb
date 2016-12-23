9e6277742d92981b55d1fc98c28f9cf7cef40c1d
rajangdavis <rajangdavis@gmail.com>
2016-12-23 14:32:33 -0800

    video thing is there

--- app/models/answer.rb
+++ app/models/answer.rb
@@ -80,7 +80,10 @@ class Answer < ActiveRecord::Base
 	end
 
 	def clean_tagline
-		clean_tagline = self.tagline.gsub(/\[red\]/,'<span class="red">').gsub(/\[red\]/,'<span class="red">').gsub(/\[\/red\]/,'</span>').gsub(/\[blue\]/,'<span class="blue">').gsub(/\[\/blue\]/,'</span>').gsub(/\[green\]/,'<span class="green">').gsub(/\[\/reen\]/,'</span>').gsub(/\[yellow\]/,'<span class="yellow">').gsub(/\[\/yellow\]/,'</span>').gsub(/\[orange\]/,'<span class="orange">').gsub(/\[\/orange\]/,'</span>').gsub(/\[lgreen\]/,'<span class="lgreen">').gsub(/\[\/lgreen\]/,'</span>').gsub(/\[bold\]/,'<strong>').gsub(/\[\/bold\]/,'</strong>').gsub(/\[i\]/,'<em>').gsub(/\[\/i\]/,'</em>').gsub(/\[u\]/,'<u>').gsub(/\[\/u\]/,'</u>').gsub(/\[br\]/,'<br>').gsub(/\[note\]/,'<div class="inst information_new"><i class="fa fa-3x fa-info-circle"></i><span information_text>').gsub(/\[\/note\]/,'</span></div>').gsub(/\[cat\]/,'<div class="inst cautiondiv_new"><i class="fa fa-3x fa-exclamation-triangle"></i>').gsub(/\[\/cat\]/,'</div>').gsub(/(https|http):\/\/qsee.custhelp.com/,'')
+		clean_tagline = self.tagline.gsub(/\[red\]/,'<span class="red">').gsub(/\[\/red\]/,'</span>').gsub(/\[blue\]/,'<span class="blue">').gsub(/\[\/blue\]/,'</span>').gsub(/\[green\]/,'<span class="green">').gsub(/\[\/reen\]/,'</span>').gsub(/\[yellow\]/,'<span class="yellow">').gsub(/\[\/yellow\]/,'</span>').gsub(/\[orange\]/,'<span class="orange">').gsub(/\[\/orange\]/,'</span>').gsub(/\[lgreen\]/,'<span class="lgreen">').gsub(/\[\/lgreen\]/,'</span>').gsub(/\[bold\]/,'<strong>').gsub(/\[\/bold\]/,'</strong>').gsub(/\[i\]/,'<em>').gsub(/\[\/i\]/,'</em>').gsub(/\[u\]/,'<u>').gsub(/\[\/u\]/,'</u>').gsub(/\[br\]/,'<br>').gsub(/\[note\]/,'<div class="inst information_new"><i class="fa fa-3x fa-info-circle"></i><span information_text>').gsub(/\[\/note\]/,'</span></div>').gsub(/\[cat\]/,'<div class="inst cautiondiv_new"><i class="fa fa-3x fa-exclamation-triangle"></i>').gsub(/\[\/cat\]/,'</div>').gsub(/(https|http):\/\/qsee.custhelp.com/,'')
+
+		clean_tagline = video_clean(clean_tagline)
+
 		clean_tagline = clean_tagline.html_safe
 		clean_tagline
 	end
@@ -89,7 +92,7 @@ class Answer < ActiveRecord::Base
 		if self.tagline_jp.blank?
 			clean_tagline = nil
 		else
-			clean_tagline = self.tagline_jp.gsub(/\[red\]/,'<span class="red">').gsub(/\[red\]/,'<span class="red">').gsub(/\[\/red\]/,'</span>').gsub(/\[blue\]/,'<span class="blue">').gsub(/\[\/blue\]/,'</span>').gsub(/\[green\]/,'<span class="green">').gsub(/\[\/reen\]/,'</span>').gsub(/\[yellow\]/,'<span class="yellow">').gsub(/\[\/yellow\]/,'</span>').gsub(/\[orange\]/,'<span class="orange">').gsub(/\[\/orange\]/,'</span>').gsub(/\[lgreen\]/,'<span class="lgreen">').gsub(/\[\/lgreen\]/,'</span>').gsub(/\[bold\]/,'<strong>').gsub(/\[\/bold\]/,'</strong>').gsub(/\[i\]/,'<em>').gsub(/\[\/i\]/,'</em>').gsub(/\[u\]/,'<u>').gsub(/\[\/u\]/,'</u>').gsub(/\[br\]/,'<br>').gsub(/\[note\]/,'<div class="inst information_new"><i class="fa fa-3x fa-info-circle"></i><span information_text>').gsub(/\[\/note\]/,'</span></div>').gsub(/\[cat\]/,'<div class="inst cautiondiv_new"><i class="fa fa-3x fa-exclamation-triangle"></i>').gsub(/\[\/cat\]/,'</div>').gsub(/(https|http):\/\/qsee.custhelp.com/,'')	
+			clean_tagline = self.tagline_jp.gsub(/\[red\]/,'<span class="red">').gsub(/\[\/red\]/,'</span>').gsub(/\[blue\]/,'<span class="blue">').gsub(/\[\/blue\]/,'</span>').gsub(/\[green\]/,'<span class="green">').gsub(/\[\/reen\]/,'</span>').gsub(/\[yellow\]/,'<span class="yellow">').gsub(/\[\/yellow\]/,'</span>').gsub(/\[orange\]/,'<span class="orange">').gsub(/\[\/orange\]/,'</span>').gsub(/\[lgreen\]/,'<span class="lgreen">').gsub(/\[\/lgreen\]/,'</span>').gsub(/\[bold\]/,'<strong>').gsub(/\[\/bold\]/,'</strong>').gsub(/\[i\]/,'<em>').gsub(/\[\/i\]/,'</em>').gsub(/\[u\]/,'<u>').gsub(/\[\/u\]/,'</u>').gsub(/\[br\]/,'<br>').gsub(/\[note\]/,'<div class="inst information_new"><i class="fa fa-3x fa-info-circle"></i><span information_text>').gsub(/\[\/note\]/,'</span></div>').gsub(/\[cat\]/,'<div class="inst cautiondiv_new"><i class="fa fa-3x fa-exclamation-triangle"></i>').gsub(/\[\/cat\]/,'</div>').gsub(/(https|http):\/\/qsee.custhelp.com/,'')	
 			clean_tagline = clean_tagline.html_safe
 		end
 		clean_tagline
@@ -99,10 +102,26 @@ class Answer < ActiveRecord::Base
 		if self.tagline_fr.blank?
 			clean_tagline = nil
 		else
-			clean_tagline = self.tagline_fr.gsub(/\[red\]/,'<span class="red">').gsub(/\[red\]/,'<span class="red">').gsub(/\[\/red\]/,'</span>').gsub(/\[blue\]/,'<span class="blue">').gsub(/\[\/blue\]/,'</span>').gsub(/\[green\]/,'<span class="green">').gsub(/\[\/reen\]/,'</span>').gsub(/\[yellow\]/,'<span class="yellow">').gsub(/\[\/yellow\]/,'</span>').gsub(/\[orange\]/,'<span class="orange">').gsub(/\[\/orange\]/,'</span>').gsub(/\[lgreen\]/,'<span class="lgreen">').gsub(/\[\/lgreen\]/,'</span>').gsub(/\[bold\]/,'<strong>').gsub(/\[\/bold\]/,'</strong>').gsub(/\[i\]/,'<em>').gsub(/\[\/i\]/,'</em>').gsub(/\[u\]/,'<u>').gsub(/\[\/u\]/,'</u>').gsub(/\[br\]/,'<br>').gsub(/\[note\]/,'<div class="inst information_new"><i class="fa fa-3x fa-info-circle"></i><span information_text>').gsub(/\[\/note\]/,'</span></div>').gsub(/\[cat\]/,'<div class="inst cautiondiv_new"><i class="fa fa-3x fa-exclamation-triangle"></i>').gsub(/\[\/cat\]/,'</div>').gsub(/(https|http):\/\/qsee.custhelp.com/,'')	
+			clean_tagline = self.tagline_fr.gsub(/\[red\]/,'<span class="red">').gsub(/\[\/red\]/,'</span>').gsub(/\[blue\]/,'<span class="blue">').gsub(/\[\/blue\]/,'</span>').gsub(/\[green\]/,'<span class="green">').gsub(/\[\/reen\]/,'</span>').gsub(/\[yellow\]/,'<span class="yellow">').gsub(/\[\/yellow\]/,'</span>').gsub(/\[orange\]/,'<span class="orange">').gsub(/\[\/orange\]/,'</span>').gsub(/\[lgreen\]/,'<span class="lgreen">').gsub(/\[\/lgreen\]/,'</span>').gsub(/\[bold\]/,'<strong>').gsub(/\[\/bold\]/,'</strong>').gsub(/\[i\]/,'<em>').gsub(/\[\/i\]/,'</em>').gsub(/\[u\]/,'<u>').gsub(/\[\/u\]/,'</u>').gsub(/\[br\]/,'<br>').gsub(/\[note\]/,'<div class="inst information_new"><i class="fa fa-3x fa-info-circle"></i><span information_text>').gsub(/\[\/note\]/,'</span></div>').gsub(/\[cat\]/,'<div class="inst cautiondiv_new"><i class="fa fa-3x fa-exclamation-triangle"></i>').gsub(/\[\/cat\]/,'</div>').gsub(/(https|http):\/\/qsee.custhelp.com/,'')
 			clean_tagline = clean_tagline.html_safe
 		end
 		clean_tagline
+	end
+
+	def video_clean(input)
+		video_file = input.match('filename="(.*?)"').to_s.gsub(/filename=/,'').gsub(/"/,'')
+
+		video_folder = input.match('folder="(.*?)"').to_s.gsub(/folder=/,'').gsub(/"/,'')
+
+		video_html = input.gsub(/\[video /,'<div class="text-center embed-responsive embed-responsive-16by9">')
+
+		video_html = video_html.gsub(/filename="(.*?)"/,'')
+
+		video_html = video_html.gsub(/folder="(.*?)"/,'<video class="embed-responsive-item" controls="controls" autoplay="autoplay" poster="'+video_folder+'html5video/'+video_file+'.jpg" style="width:100%" title="recoverpassword" loop="loop" onended="var v=this;setTimeout(function(){v.play()},300)"><source src="'+video_folder+'html5video/'+video_file+'.m4v" type="video/mp4" /><source src="'+video_folder+'html5video/'+video_file+'.webm" type="video/webm" /><source src="'+video_folder+'html5video/'+video_file+'.ogv" type="video/ogg" /><source src="'+video_folder+'html5video/'+video_file+'.mp4" />')
+
+		video_html = video_html.gsub(/\]/,'</video></div>').html_safe
+
+		video_html
 	end
 
 	def word_count
